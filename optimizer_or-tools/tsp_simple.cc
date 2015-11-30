c0dcfb56e5cb7671dcb93d92c21da486edcea626
braktar <rgwenael@gmail.com>
2015-11-30 15:20:01 +0100

    Support multi-véhicule, multi skill, multi capacités

--- optimizer_or-tools/tsp_simple.cc
+++ optimizer_or-tools/tsp_simple.cc
@@ -29,6 +29,15 @@
 
 DEFINE_int64(time_limit_in_ms, 2000, "Time limit in ms, 0 means no limit.");
 DEFINE_int64(soft_upper_bound, 3, "Soft upper bound multiplicator, 0 means hard limit.");
+DEFINE_string(vehicle_file, "", "File containing all the vehicles infos");
+DEFINE_bool(skip_node, false, "Allow to skip some customers from the plan");
+DEFINE_int64(back_to_depot, 0, "Times a vehicle could come back reloading at the depot");
+DEFINE_int64(fix_cost_skip_node, 5000, "Fixed Cost if a node is skipped");
+DEFINE_int64(linear_cost_skip_node, 1000, "linear cost if a node node is skipped depending on the first dimension");
+
+const char* kCapacity = "Capacity";
+const char* kTime = "Time";
+const char* kDistance = "Distance";
 
 namespace operations_research {
 
@@ -37,23 +46,68 @@ void TSPTWSolver(const TSPTWDataDT & data) {
   const int size = data.Size();
   const int size_matrix = data.SizeMatrix();
   const int size_rest = data.SizeRest();
-  const int64 fixedVehicleCost = 500;
 
+  const RoutingModel::NodeIndex kDepot(0);
+
+  // Définition des départs et arrivées par véhicule
   std::vector<std::pair<RoutingModel::NodeIndex, RoutingModel::NodeIndex>> *start_ends = new std::vector<std::pair<RoutingModel::NodeIndex, RoutingModel::NodeIndex>>(1);
-  (*start_ends)[0] = std::make_pair(data.Start(), data.Stop());
-  RoutingModel routing(size, 1, *start_ends);
-  routing.SetCost(NewPermanentCallback(&data, &TSPTWDataDT::Distance));
+  for( int i = 0; i < data.SizeVehicle(); ++i ) {
+	(*start_ends)[i] = std::make_pair(data.Start(), data.Stop());
+  }
 
+  RoutingModel routing(size, data.SizeVehicle()*(1+FLAGS_back_to_depot),*start_ends);
+  //routing.SetDepot(kDepot);
+
+  const int64 kNullCapacitySlack = 0;
   const int64 horizon = data.Horizon();
+  const int64 capacity_horizon_0 = data.Capacity_Horizon(0);
+  const int64 capacity_horizon_1 = data.Capacity_Horizon(1);
+
+  routing.AddDimension(NewPermanentCallback(&data, &TSPTWDataDT::Distance),
+    horizon, horizon, true, kDistance);
   routing.AddDimension(NewPermanentCallback(&data, &TSPTWDataDT::TimePlusServiceTime),
-    horizon, horizon, true, "time");
+    horizon, horizon, true, kTime);
 
-  routing.SetFixedCostOfAllVehicles(fixedVehicleCost);
+	if(data.SizeCapacities() > 0)
+		routing.AddDimension(NewPermanentCallback(&data, &TSPTWDataDT::Demand0),
+			kNullCapacitySlack, capacity_horizon_0, true, kCapacity+std::to_string(0));
+	if(data.SizeCapacities() > 1)
+		routing.AddDimension(NewPermanentCallback(&data, &TSPTWDataDT::Demand1),
+			kNullCapacitySlack, capacity_horizon_1, true, kCapacity+std::to_string(1));
 
-  //  Setting time windows
+  // Setting Vehicles
+  for(int i = 0; i < data.SizeVehicle(); ++i){
+	  routing.SetFixedCostOfVehicle(data.FixedCostVehicle(i),i);
+  }
+
+  const RoutingDimension& time_dimension = routing.GetDimensionOrDie(kTime);
+
+  for(int vehc = 0; vehc < data.SizeVehicle(); vehc++) {
+		time_dimension.CumulVar(vehc)->SetMin(data.ReadyTimeVehicle(vehc));
+		routing.AddVariableMinimizedByFinalizer(time_dimension.CumulVar(vehc));
+		//routing.GetMutableDimension(kTime)->SetSpanCostCoefficientForVehicle(data.VehicleDistanceCost(vehc), vehc);
+
+		routing.GetMutableDimension(kTime)->SetEndCumulVarSoftUpperBound(vehc, data.FinishTimeVehicle(vehc),FLAGS_soft_upper_bound);
+
+		for(int i = 0; i< data.SizeCapacities(); ++i){
+			const RoutingDimension& capacity_dimension = routing.GetDimensionOrDie(kCapacity+std::to_string(i));
+			routing.AddVariableMinimizedByFinalizer(capacity_dimension.CumulVar(vehc));
+			capacity_dimension.CumulVar(vehc)->SetMax(data.CapacitiesVehicle(vehc,0));
+		}
+  }
+
+  //routing.SetArcCostEvaluatorOfAllVehicles(NewPermanentCallback(&data, &TSPTWDataDT::Distance));
+
+  // Restriction des clients qu'un véhicule peut livrer (skill)
+	if(data.SizeSkills() > 0)
+		for(RoutingModel::NodeIndex i(1); i < size_matrix - 1; ++i)
+			for(int vehc = 0; vehc < data.SizeVehicle(); ++vehc)
+				if(!data.NodePerformedbyVehicle(i, vehc%data.SizeVehicle()))
+					routing.VehicleVar(routing.NodeToIndex(i))->RemoveValue(vehc);
+		//  Setting time windows
   for (RoutingModel::NodeIndex i(1); i < size_matrix - 1; ++i) {
     int64 index = routing.NodeToIndex(i);
-    IntVar* const cumul_var = routing.CumulVar(index, "time");
+    IntVar* const cumul_var = routing.CumulVar(index, kTime);
     int64 const ready = data.ReadyTime(i);
     int64 const due = data.DueTime(i);
 
@@ -61,15 +115,15 @@ void TSPTWSolver(const TSPTWDataDT & data) {
       std::vector<RoutingModel::NodeIndex> *vect = new std::vector<RoutingModel::NodeIndex>(1);
       (*vect)[0] = i;
       routing.AddDisjunction(*vect, 0); // skip node for free
-        cumul_var->SetMin(0);
-        cumul_var->SetMax(0);
+      cumul_var->SetMin(0);
+      cumul_var->SetMax(0);
     } else if (ready > 0 || due > 0) {
       if (ready > 0) {
         cumul_var->SetMin(ready);
       }
       if (due > 0 && due < 2147483647) {
         if (FLAGS_soft_upper_bound > 0) {
-          routing.SetCumulVarSoftUpperBound(i, "time", due, FLAGS_soft_upper_bound);
+          routing.SetCumulVarSoftUpperBound(i, kTime, due, FLAGS_soft_upper_bound);
         } else {
           cumul_var->SetMax(due);
         }
@@ -77,7 +131,11 @@ void TSPTWSolver(const TSPTWDataDT & data) {
 
       std::vector<RoutingModel::NodeIndex> *vect = new std::vector<RoutingModel::NodeIndex>(1);
       (*vect)[0] = i;
-      routing.AddDisjunction(*vect, 100000);
+			if(FLAGS_skip_node)
+				if(data.SizeCapacities()>0)
+					routing.AddDisjunction(*vect, data.Demand0(i,i)*FLAGS_linear_cost_skip_node+FLAGS_fix_cost_skip_node);
+				else
+					routing.AddDisjunction(*vect, 1*FLAGS_linear_cost_skip_node+FLAGS_fix_cost_skip_node);
     } else {
       std::vector<RoutingModel::NodeIndex> *vect = new std::vector<RoutingModel::NodeIndex>(1);
       (*vect)[0] = i;
@@ -93,7 +151,7 @@ void TSPTWSolver(const TSPTWDataDT & data) {
       (*vect)[p] = i;
 
       int64 index = routing.NodeToIndex(i);
-      IntVar* const cumul_var = routing.CumulVar(index, "time");
+      IntVar* const cumul_var = routing.CumulVar(index, kTime);
       int64 const ready = data.ReadyTime(rest);
       int64 const due = data.DueTime(rest);
 
@@ -102,7 +160,7 @@ void TSPTWSolver(const TSPTWDataDT & data) {
       }
       if (due > 0 && due < 2147483647) {
         if (FLAGS_soft_upper_bound > 0) {
-          routing.SetCumulVarSoftUpperBound(i, "time", due, FLAGS_soft_upper_bound);
+          routing.SetCumulVarSoftUpperBound(i, kTime, due, FLAGS_soft_upper_bound);
         } else {
           cumul_var->SetMax(due);
         }
@@ -134,12 +192,28 @@ void TSPTWSolver(const TSPTWDataDT & data) {
 
   Solver *solver = routing.solver();
 
+  // Definition des rechargements au depot
+  for(int vehc = 14; vehc < data.SizeVehicle()*(1+FLAGS_back_to_depot); vehc++) {
+    routing.AddVariableMinimizedByFinalizer(time_dimension.CumulVar(vehc - data.SizeVehicle()));
+		solver->MakeLessOrEqual(time_dimension.CumulVar(vehc), time_dimension.CumulVar(vehc - data.SizeVehicle()));
+		//routing.GetMutableDimension(kTime)->SetSpanCostCoefficientForVehicle(data.VehicleDistanceCost(vehc%data.SizeVehicle()), vehc);
+
+    routing.GetMutableDimension(kTime)->SetEndCumulVarSoftUpperBound(vehc, data.FinishTimeVehicle(vehc%data.SizeVehicle()),FLAGS_soft_upper_bound);
+
+		for(int i = 0; i< data.SizeCapacities(); ++i){
+			const RoutingDimension& capacity_dimension = routing.GetDimensionOrDie(kCapacity+std::to_string(i));
+			routing.AddVariableMinimizedByFinalizer(capacity_dimension.CumulVar(vehc - data.SizeVehicle()));
+			solver->MakeLessOrEqual(capacity_dimension.CumulVar(vehc), capacity_dimension.CumulVar(vehc - data.SizeVehicle()));
+			capacity_dimension.CumulVar(vehc)->SetMax(data.CapacitiesVehicle(vehc%data.SizeVehicle(),0));
+		}
+  }
+
   const Assignment* solution = routing.Solve(NULL);
 
   if (solution != NULL) {
     std::cout << "Cost: " << solution->ObjectiveValue() << std::endl;
     TSPTWSolution sol(data, &routing, solution);
-    for(int route_nbr = 0; route_nbr < routing.vehicles(); route_nbr++) {
+    for(int route_nbr = 0; route_nbr < data.SizeVehicle()*(1+FLAGS_back_to_depot); route_nbr++) {
       for (int64 index = routing.Start(route_nbr); !routing.IsEnd(index); index = solution->Value(routing.NextVar(index))) {
         RoutingModel::NodeIndex nodeIndex = routing.IndexToNode(index);
         nodeIndex = data.RestShiftValue(nodeIndex.value());
@@ -156,8 +230,14 @@ void TSPTWSolver(const TSPTWDataDT & data) {
 
 int main(int argc, char **argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
-  operations_research::TSPTWDataDT tsptw_data(FLAGS_instance_file);
-  operations_research::TSPTWSolver(tsptw_data);
+
+	if(FLAGS_vehicle_file == ""){
+		operations_research::TSPTWDataDT tsptw_data(FLAGS_instance_file);
+		operations_research::TSPTWSolver(tsptw_data);
+  }else{
+		operations_research::TSPTWDataDT tsptw_data(FLAGS_instance_file, FLAGS_vehicle_file);
+		operations_research::TSPTWSolver(tsptw_data);
+	}
 
   return 0;
 }
--- optimizer_or-tools/tsptw_data_dt.h
+++ optimizer_or-tools/tsptw_data_dt.h
@@ -18,10 +18,19 @@ namespace operations_research {
 class TSPTWDataDT : public RoutingDataDT {
 public:
   explicit TSPTWDataDT(std::string filename) : RoutingDataDT(0), instantiated_(false) {
+		LoadVehicles();
+    LoadInstance(filename);
+    SetRoutingDataInstanciated();
+  }
+  explicit TSPTWDataDT(std::string filename, std::string vehicle_file) : RoutingDataDT(0), instantiated_(false), v_instantiated_(false) {
+
+		LoadVehicles(vehicle_file);
     LoadInstance(filename);
     SetRoutingDataInstanciated();
   }
   void LoadInstance(const std::string & filename);
+  void LoadVehicles(const std::string & vehicle_file);
+  void LoadVehicles();
   
   void SetStart(RoutingModel::NodeIndex s) {
     CHECK_LT(s, Size());
@@ -45,6 +54,13 @@ public:
     return horizon_;
   }
 
+	int64 Capacity_Horizon(int64 i) const {
+		if (capacity_horizon_-> size()> i)
+			return capacity_horizon_->at(i);
+		else
+			return 0;
+  }
+
   int64 RestShiftValue(int64 value) const {
     if (value < SizeMatrix()) {
       return value;
@@ -53,6 +69,35 @@ public:
       return (value - SizeMatrix()) / SizeMatrix() + SizeMatrix();
     }
   }
+
+  int64 ReadyTimeVehicle(int64 i) const {
+	return tsptw_vehicles_[i].ready_time;
+  }
+
+  int64 FinishTimeVehicle(int64 i) const {
+	return tsptw_vehicles_[i].finish_time;
+  }
+
+  bool SkillVehicle(int64 i, int64 j) const {
+	return tsptw_vehicles_[i].skills->at(j);
+  }
+
+  int64 CapacitiesVehicle(int64 i, int64 j) const {
+	return tsptw_vehicles_[i].capacities->at(j);
+  }
+
+  int64 FixedCostVehicle(int64 i) const {
+	return tsptw_vehicles_[i].fixed_cost;
+  }
+
+  int64 DistanceCostVehicle(int64 i) const {
+	return tsptw_vehicles_[i].distance_cost;
+  }
+
+  bool NodeNeedThisSkill(RoutingModel::NodeIndex i, int64 s) const {
+	return tsptw_clients_[RestShiftValue(i.value())].need_skill->at(s);
+  }
+
 
   int64 ReadyTime(RoutingModel::NodeIndex i) const {
     return tsptw_clients_[RestShiftValue(i.value())].ready_time;
@@ -66,8 +111,28 @@ public:
     return tsptw_clients_[RestShiftValue(i.value())].service_time;
   }
 
-  int64 Demand(RoutingModel::NodeIndex i) const {
-    return tsptw_clients_[RestShiftValue(i.value())].demand;
+  int64 Demand0(RoutingModel::NodeIndex from, RoutingModel::NodeIndex to) const { // Duplicate to add Capacity Dimension
+    return tsptw_clients_[RestShiftValue(from.value())].demand->at(0);
+  }
+
+  int64 Demand1(RoutingModel::NodeIndex from, RoutingModel::NodeIndex to) const {
+    return tsptw_clients_[RestShiftValue(from.value())].demand->at(1);
+  }
+
+  /*int64 DemandThisCapacity(RoutingModel::NodeIndex to, int capacity) const {
+	return tsptw_clients_[RestShiftValue(to.value())].demand->at(capacity);
+  }*/
+
+  bool NodePerformedbyVehicle(RoutingModel::NodeIndex to, int vehicle) const {
+  bool temp = true;
+  for(int64 i = 0; i < SizeSkills(); ++i)
+		if(NodeNeedThisSkill(to, i) && !SkillVehicle(vehicle, i))
+			temp = false;
+  return temp;
+  }
+
+  int64 VehicleDistanceCost(int64 vehicle) const {
+		return tsptw_vehicles_[vehicle].distance_cost;
   }
 
   // Override
@@ -143,11 +208,25 @@ public:
   int32 SizeRest() const {
     return size_rest_;
   }
+
+  int32 SizeVehicle() const {
+    return size_vehicle_;
+  }
+
+  int32 SizeSkills() const {
+    return nSkills_;
+  }
+
+  int32 SizeCapacities() const {
+    return nCapacities_;
+  }
   
 private:
   int32 size_matrix_;
   int32 size_rest_;
+  int32 size_vehicle_;
   void ProcessNewLine(char* const line);
+  void ProcessNewLineVehicle(char* const line);
   void InitLoadInstance() {
     line_number_ = 0;
     visualizable_ = false;
@@ -156,7 +235,10 @@ private:
     name_ = "";
     comment_ = "";
   }
-  
+  void InitLoadVehicles() {
+    v_line_number_ = 0;
+  }
+
   //  Helper function
   int64& SetMatrix(int i, int j) {
     return distances_.Cost(RoutingModel::NodeIndex(i), RoutingModel::NodeIndex(j));
@@ -168,32 +250,57 @@ private:
 
 
   bool instantiated_;
+  bool v_instantiated_;
   RoutingModel::NodeIndex start_, stop_;
   struct TSPTWClient {
-    TSPTWClient(int cust_no, double d, double r_t, double d_t, double s_t) :
-    customer_number(cust_no), demand(d), ready_time(r_t), due_time(d_t), service_time(s_t) {}
-    TSPTWClient(int cust_no, double r_t, double d_t):
-    customer_number(cust_no), demand(0.0), ready_time(r_t), due_time(d_t), service_time(0.0){
+  TSPTWClient(int cust_no, std::vector<int64>* d, double r_t, double d_t, double s_t, std::vector<bool> * s) :
+    customer_number(cust_no), demand(d), ready_time(r_t), due_time(d_t), service_time(s_t), need_skill(s) {
     }
-    TSPTWClient(int cust_no, double r_t, double d_t, double s_t):
-    customer_number(cust_no), demand(0.0), ready_time(r_t), due_time(d_t), service_time(s_t){
+  TSPTWClient(int cust_no, std::vector<int64>* d, double r_t, double d_t, double s_t) :
+    customer_number(cust_no), demand(d), ready_time(r_t), due_time(d_t), service_time(s_t), need_skill(NULL) {}
+  TSPTWClient(int cust_no, double r_t, double d_t):
+    customer_number(cust_no), demand(NULL), ready_time(r_t), due_time(d_t), service_time(0.0), need_skill(NULL){
+    }
+  TSPTWClient(int cust_no, double r_t, double d_t, double s_t):
+    customer_number(cust_no), demand(NULL), ready_time(r_t), due_time(d_t), service_time(s_t), need_skill(NULL){
     }
     int customer_number;
-    int64 demand;
+    std::vector<int64>* demand;
     int64 ready_time;
     int64 due_time;
     int64 service_time;
+    std::vector<bool> *need_skill;
+  };
+  struct TSPTWVehicle {
+		TSPTWVehicle() :
+		vehicle_number(0), fixed_cost(0), distance_cost(0), ready_time(0), finish_time(2147483647), skills(NULL), capacities(NULL){}
+		TSPTWVehicle(int v_no, int f_co, int d_co, int r_t, int f_t, std::vector<bool> *s, std::vector<int64> *c):
+		vehicle_number(v_no), fixed_cost(f_co), distance_cost(d_co), ready_time(r_t), finish_time(f_t), skills(s), capacities(c){}
+
+		int vehicle_number;
+		int fixed_cost;
+		int distance_cost;
+		int ready_time;
+		int finish_time;
+		std::vector<bool> *skills;
+    std::vector<int64> *capacities;
   };
 
   std::vector<TSPTWClient> tsptw_clients_;
+  std::vector<TSPTWVehicle> tsptw_vehicles_;
   std::string details_;
   std::string filename_;
+  std::string vehicle_file_;
   int64 horizon_;
+  std::vector<int64>* capacity_horizon_;
   bool visualizable_;
   bool two_dimension_;
   bool symmetric_;
+  int64 nSkills_;
+  int64 nCapacities_;
     
   int line_number_;
+  int v_line_number_;
   std::string comment_;
 };
 
@@ -232,6 +339,41 @@ void TSPTWDataDT::LoadInstance(const std::string & filename) {
   instantiated_ = true;
 }
 
+void TSPTWDataDT::LoadVehicles() {
+  InitLoadVehicles();
+  size_vehicle_ = 1;
+  nCapacities_ = 0;
+  nSkills_ = 0;
+  tsptw_vehicles_.push_back(TSPTWVehicle());
+  vehicle_file_ = "";
+  v_instantiated_ = true;
+}
+
+void TSPTWDataDT::LoadVehicles(const std::string & vehicle_file) {
+  InitLoadVehicles();
+  size_vehicle_ = 0;
+  nCapacities_ = 0;
+  nSkills_ = 0;
+  FileLineReader reader(vehicle_file.c_str());
+  reader.set_line_callback(NewPermanentCallback(
+													 this,
+													 &TSPTWDataDT::ProcessNewLineVehicle));
+
+  reader.Reload();
+
+  for (int32 i = 0 ; i < size_vehicle_; ++i)
+		for (int32 j = 0; j < nCapacities_; ++j)
+			capacity_horizon_[j] = std::max(capacity_horizon_[j], tsptw_vehicles_[i].capacities[j]);
+
+  if (!reader.loaded_successfully()) {
+		LOG(ERROR) << "Could not open vehicle file " << vehicle_file;
+  }
+
+  vehicle_file_ = vehicle_file;
+  v_instantiated_ = true;
+}
+
+
 void TSPTWDataDT::ProcessNewLine(char* const line) {
   ++line_number_;
 
@@ -267,18 +409,90 @@ void TSPTWDataDT::ProcessNewLine(char* const line) {
       SetTimeMatrix(line_number_ - 3, j) = static_cast<int64>(atof(words[j*2+1].c_str()));
     }
   }
-  else if (line_number_ > 2 + SizeMatrix() && line_number_ <= 2 + SizeMatrix() + Size()) {
-    CHECK_EQ(words.size(), 3) << "Time window in TSPTW instance file is ill formed : " << line_number_;
+  else if (words.size() == 3 && line_number_ > 2 + SizeMatrix() && line_number_ <= 2 + SizeMatrix() + Size()) {
     tsptw_clients_.push_back(TSPTWClient(line_number_ - 3 - SizeMatrix(),
                                          atof(words[0].c_str()),
                                          atof(words[1].c_str()),
                                          atof(words[2].c_str())
     ));
   }
+  else if (words.size() == 5 && line_number_ > 2 + SizeMatrix() && line_number_ <= 2 + SizeMatrix() + Size()) {
+    std::vector<bool>* t_s(new std::vector<bool>);
+    std::vector<int64>* t_c(new std::vector<int64>);
+		for(int i = 0; i < SizeSkills() ; ++i)
+			if(atof(words[3+SizeCapacities()+i].c_str()) == 1)
+				t_s->push_back(true);
+			else
+				t_s->push_back(false);
+		for(int i = 0; i < SizeCapacities() ; ++i)
+			t_c->push_back(atof(words[3+i].c_str())*1000);
+
+		tsptw_clients_.push_back(TSPTWClient(line_number_ - 3 - SizeMatrix(),
+																					 t_c,
+		                                       atof(words[0].c_str()),
+		                                       atof(words[1].c_str()),
+		                                       atof(words[2].c_str()),
+																					 t_s
+		));
+  }
+  else if (line_number_ > 2 + SizeMatrix() && line_number_ <= 2 + SizeMatrix() + Size()) {
+		CHECK_EQ(words.size(), 5) << "Time window in TSPTW instance file is ill formed : " << line_number_;
+  }
   else {
     VLOG(0) << "Unexpected line :" << line_number_;
   }
 }  //  void ProcessNewLine(char* const line)
+
+void TSPTWDataDT::ProcessNewLineVehicle(char* const line) {
+  ++v_line_number_;
+
+  static const char kWordDelimiters[] = " ";
+  std::vector<std::string> words = strings::Split(line, kWordDelimiters, strings::SkipEmpty());
+
+  static const int DSU_data_tokens = 7;
+  static const int DSU_last_customer = 999;
+
+  //  Empty lines
+  if (words.size() == 0) {
+    return;
+  }
+  else if (v_line_number_ == 1) {
+		size_vehicle_ = atoi32(words[0]);
+  }
+  else if (v_line_number_ == 2) {
+    nSkills_ = atoi32(words[0]);
+  }
+  else if (v_line_number_ == 3) {
+    nCapacities_ = atoi32(words[0]);
+    CHECK_LE(nCapacities_, 2) << "Too many capacities : " << nCapacities_<<"/2" ;
+    for(int i = 0; i< nCapacities_; ++i)
+			capacity_horizon_->push_back(0);
+  }
+  else if (v_line_number_ > 3 && v_line_number_ <= 3 + SizeVehicle()) {
+		CHECK_EQ(words.size(), 4 + SizeCapacities() + SizeSkills() ) << "Vehicule Configuration don't match : " << v_line_number_;
+		std::vector<bool>* t_s(new std::vector<bool>);
+    std::vector<int64>* t_c(new std::vector<int64>);
+	  for(int i = 0; i < SizeSkills() ; ++i)
+	    if(atof(words[4+SizeCapacities()+i].c_str()) == 1)
+			  t_s->push_back(true);
+	    else
+			  t_s->push_back(false);
+	  for(int i = 0; i < SizeCapacities() ; ++i)
+	    t_c->push_back(atof(words[4+i].c_str()));
+
+    tsptw_vehicles_.push_back(TSPTWVehicle(v_line_number_ - 4,
+                                           atof(words[0].c_str()),
+																					 atof(words[1].c_str()),
+		                                       atof(words[2].c_str()),
+																					 atof(words[3].c_str()),
+																					 t_s,
+																					 t_c
+    ));
+  }
+  else {
+    VLOG(0) << "Unexpected line :" << v_line_number_;
+  }
+}  //  void ProcessNewLineVehicle(char* const line)
 
 }  //  namespace operations_research
 
